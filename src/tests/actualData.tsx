b82b1d9fba4f231434c65545eec8c26886f571df
Kathleen Bongco <kathleen.bongco@gmail.com>
2024-12-19 11:27:32 -0500

    created functions

---
+++ src/tests/actualData.tsx
@@ -0,0 +1,58 @@
+// Actual Test Data of Friends found in OpenPLifting
+export const testData = [
+  {
+    // 100kg lifter
+    'bodyWeight': 98.54,
+    'totalLifted': 490,
+    'expectedDOTS': 420.71
+  },
+  {
+    // SHW lifter 
+    'bodyWeight': 111,
+    'totalLifted': 465,
+    'expectedDOTS': 382.15
+  },
+  {
+    // Light weight lifter
+    'bodyWeight': 47.7,
+    'totalLifted': 357.5,
+    'expectedDOTS': 463.10
+  },
+  {
+    // 67.5 lifter
+    'bodyWeight': 65.2,
+    'totalLifted': 427.5,
+    'expectedDOTS': 450.19
+  },
+  {
+    // 82.5 lifter
+    // Masters lifter
+    'bodyWeight': 81.7,
+    'totalLifted': 422.5,
+    'expectedDOTS': 394.00
+  },
+  {
+    // 75kg lifter
+    'bodyWeight': 71.4,
+    'totalLifted': 447.5,
+    'expectedDOTS': 447.58
+  },
+  {
+    // 82.5kg lifter national champion
+    'bodyWeight': 80.4,
+    'totalLifted': 537.5,
+    'expectedDOTS': 505.21
+  },
+  {
+    // 56kg lifter
+    'bodyWeight': 54.1,
+    'totalLifted': 402.5,
+    'expectedDOTS': 477.46
+  },
+  {
+    // 60kg lifter
+    'bodyWeight': 59.2,
+    'totalLifted': 417.5,
+    'expectedDOTS': 466.7
+  }
+]
